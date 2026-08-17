3a1e35a662994a3fb8ff7cde6a1b5bbfb15d4fe3
Vedant Satpute <satpute.vedant.btech2025@sitpune.edu.in>
2026-08-17 15:11:18 +0530

    Delete CollegeManagement.java

--- CollegeManagement.java
+++
@@ -1,8 +0,0 @@
-import packagess.StudentDetails;
-
-public class CollegeManagement {
-    public static void main(String[] args) {
-        StudentDetails student = new StudentDetails(21, "Rahul", "Computer Science");
-        student.display();
-    }
-}
